ee0bade229ddd407d0cd1321c895a3cca705d079
coderAndrew37 <omolloandrew37@gmail.com>
2024-10-22 20:03:03 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,13 @@ const path = require("path");
 
 require("express-async-errors");
 
+const cors = require("cors");
+
 app.use(
   cors({
     origin: [
-      "https://massage-2-q1w93y11n-coderandrew37s-projects.vercel.app", // Your Vercel frontend domain
-      "https://massage-2.vercel.app", // Another possible domain on Vercel
+      "https://massage-2-6xo1wesm3-coderandrew37s-projects.vercel.app", // Vercel frontend
+      "http://127.0.0.1:5500", // Local development
     ],
     credentials: true, // Allow credentials (cookies, etc.)
   })
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,4 @@
 export const baseURL =
   window.location.hostname === "localhost"
     ? "http://localhost:5500" // Local development API
-    : "https://massage-2-q1w93y11n-coderandrew37s-projects.vercel.app/api"; // Production API
+    : "https://massage-2-6xo1wesm3-coderandrew37s-projects.vercel.app/api"; // Production API URL
