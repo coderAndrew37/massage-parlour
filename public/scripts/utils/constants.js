64e51d94dc1d8450bd941ee94151ed64c1c79a2e
coderAndrew37 <omolloandrew37@gmail.com>
2024-10-31 23:11:29 +0300

    Moving the static files to the public folder

--- public/scripts/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const baseURL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:5500" // Local development API
-    : "https://massage-2-6xo1wesm3-coderandrew37s-projects.vercel.app/api"; // Production API URL
